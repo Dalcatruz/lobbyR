afa158f1d61bf8c82cedcaf0799c0e5db04ac7c9
Daniel Alcatruz <nikolas20026@gmail.com>
2019-08-30 05:00:42 -0400

    First Commit

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' My Hello CpyE function
+#'
+#' @param x The name of the person to say hi to.
+#'
+#' @return the output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("Sergio Toro)
+#' \dontrun{
+#' hello("Steve")
+#' }
+#'
+hello <- function(x) {
+  print(paste0("Hola amigo/a ",x, "bienvenido a comportamiento politico y electoral de APCP"))
+}
