5762a6091d13dfc9dd211ec4303a372d5321465e
Daniel Alcatruz <nikolas20026@gmail.com>
2019-08-31 19:11:35 -0400

    Data political interest.

---
+++ R/data_political_juv.R
@@ -0,0 +1,11 @@
+#' @title Data for anallysis of political interests of the youth
+#'
+#' @description A data \code{tibble} that is for multiples analysis
+#'
+#' @format A list of principal variables:
+#' \describe{
+#' \item{P38}{Cursando o no cursando el colegio }
+#' \item{P52_x}{Confianza en las intittuciones, enumeración de 10 instituciones desde P52_1 hasta P52_10 }
+#' }
+"data_political_juv"
+
--- R/political_jus_read.R
+++ R/political_jus_read.R
@@ -6,7 +6,7 @@
 #' @export
 #' @importFrom readr read_csv
 #' @examples
-#' data_political= ystem.file("extdata","data_political_juv.csv",package = "compol")
+#' data_political= system.file("extdata","data_political_juv.csv",package = "compol")
 #' political_read_data(csv)
 political_read_data= function(path){
 readr::read_csv(path)
