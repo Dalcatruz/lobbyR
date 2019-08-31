56ca0c59cfdcdd42babe2013fc9ca4ceab87a2b4
Daniel Alcatruz <nikolas20026@gmail.com>
2019-08-31 19:00:53 -0400

    Add new data of political interest of the youth.

---
+++ R/political_jus_read.R
@@ -0,0 +1,13 @@
+#' Read CSV duplicate (political data juv)
+#'
+#' @param path  path to filename
+#'
+#' @return a \code{tibble}
+#' @export
+#' @importFrom readr read_csv
+#' @examples
+#' data_political= ystem.file("extdata","data_political_juv.csv",package = "compol")
+#' political_read_data(csv)
+political_read_data= function(path){
+readr::read_csv(path)
+}
