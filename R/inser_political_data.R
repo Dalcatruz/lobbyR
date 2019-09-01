c35c39c0b118154af956a51664c84f18c8688e4d
Daniel Alcatruz <nikolas20026@gmail.com>
2019-08-31 22:52:51 -0400

    a

--- R/inser_political_data.R
+++ R/inser_political_data.R
@@ -1,7 +1,8 @@
 library(readr)
 library(tibble)
-data_political_juv <- read_csv("data-raw/base_analysis.csv")
+data_political_juv <- read_csv("base_analysis.csv")
+## code to prepare `DATASET` dataset goes here
+
 
 
 readr::write_csv(data_political_juv,path = "inst/extdata/data_political_juv.csv")
-
