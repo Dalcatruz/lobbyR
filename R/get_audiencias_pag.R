326f7660b42bb21ac4326148c1baad27e06732fc
Daniel Alcatruz <nikolas20026@gmail.com>
2019-09-25 18:51:49 -0300

    Update_Audiencias

--- R/get_audiencias_pag.R
+++ R/get_audiencias_pag.R
@@ -1,8 +1,9 @@
 
 
 #' Obtención de audiencias
+#' @param ini_page Es el número de pagina inicial de audiencias
 #'
-#' @param num_page Es el número de paginas de audiencias a retornar, la API devuelve de a 10 audiencias por pagina
+#' @param num_page Es el número de pagina final de audiencias
 #'
 #' @return Un \dataframe con las audiencias estructuradas
 #'
@@ -12,13 +13,13 @@
 #' @examples
 #' get_audiencias_pag(num_page=3)
 #'
-get_audiencias_pag<- function(num_page){
+get_audiencias_pag<- function(ini_page,num_page){
 
   url<-"https://www.leylobby.gob.cl/api/v1/audiencias?&page="
 
   data_audiencias<- data.frame()
 
-  for (i in 1:num_page) {
+  for (i in ini_page:num_page) {
 
     acum<-jsonlite::fromJSON(paste0(url,i), flatten = TRUE)$data
 
